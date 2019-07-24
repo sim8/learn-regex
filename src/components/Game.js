d773dd3a8865c0667dd62aa5747ae06ec1c08d88
sim8 <simeon.k.lees@gmail.com>
2019-07-25 00:12:21 +0100

    Add basic deps and components

---
+++ src/components/Game.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainInput from "./MainInput";
+
+function Game() {
+  return (
+    <div className="game">
+      <MainInput />{" "}
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/components/MainInput.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MainInput() {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default MainInput;
