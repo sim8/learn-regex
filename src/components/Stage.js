7fc6cc3a45a284e12681ddcd0d6e9cce9d9f0871
sim8 <simeon.k.lees@gmail.com>
2019-07-30 10:38:51 +0100

    Basic styling

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import styled from "styled-components";
 import Test from "./Test";
 import Button from "./styled/Button";
 import {
@@ -11,6 +12,19 @@ import {
   moveToNextStage,
   moveToPreviousStage
 } from "../actions/progressActions";
+
+const StageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  height: 100vh;
+  margin-left: 20px;
+`;
+
+const NavButtons = styled.div`
+  height: 70px;
+  padding: 100px 0;
+`;
 
 const mapStateToProps = state => ({
   stageConfig: getStageConfig(state),
@@ -27,18 +41,20 @@ function Stage({
 }) {
   const { type, text, ...config } = stageConfig;
   return (
-    <div className="stage">
+    <StageWrapper>
       {text.map((t, index) => (
         <p key={index}>{t}</p>
       ))}
       {type === "TEST" ? <Test {...config} /> : null}
-      {canMoveToPreviousStage && <Button onClick={onClickBack}>BACK</Button>}
-      {canMoveToNextStage && (
-        <Button type="primary" onClick={onClickNext}>
-          CONTINUE
-        </Button>
-      )}
-    </div>
+      <NavButtons>
+        {canMoveToPreviousStage && <Button onClick={onClickBack}>BACK</Button>}
+        {canMoveToNextStage && (
+          <Button type="primary" onClick={onClickNext}>
+            CONTINUE
+          </Button>
+        )}
+      </NavButtons>
+    </StageWrapper>
   );
 }
 export default connect(
