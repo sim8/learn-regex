152ef8548899e7aee7c1ad24dac39eb7c445fdd5
sim8 <simeon.k.lees@gmail.com>
2019-09-02 20:36:26 +0100

    Add title

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Head from "next/head";
 import Stage from "../components/Stage";
 
 function App() {
   return (
     <div className="App">
+      <Head>
+        <title>Learn Regex</title>
+      </Head>
       <Stage />
     </div>
   );
