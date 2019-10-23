52964810ddc3094cf6b620d5424c03a25cc981be
sim8 <simeon.k.lees@gmail.com>
2019-10-22 20:38:32 -0400

    Fix events

--- actions/progressActions.js
+++ actions/progressActions.js
@@ -7,28 +7,30 @@ import {
   ACTIONS as TRACKING_ACTIONS,
   CATEGORIES as TRACKING_CATEGORIES
 } from "../constants/trackingConstants";
+import { getStageId } from "../selectors/progressSelectors";
+import { event } from "../lib/gtag";
 
-export const moveToNextStage = () => {
+export const moveToNextStage = () => (dispatch, getState) => {
+  const stageId = getStageId(getState());
   event(TRACKING_ACTIONS.MOVE_TO_NEXT_STAGE, {
     category: TRACKING_CATEGORIES.STAGE,
     label: stageId
   });
-  return {
+  dispatch({
     type: MOVE_TO_NEXT_STAGE
-  };
+  });
 };
 
-export const moveToPreviousStage = () => {
+export const moveToPreviousStage = () => (dispatch, getState) => {
+  const stageId = getStageId(getState());
   event(TRACKING_ACTIONS.MOVE_TO_PREVIOUS_STAGE, {
     category: TRACKING_CATEGORIES.STAGE,
     label: stageId
   });
-  return {
+  dispatch({
     type: MOVE_TO_PREVIOUS_STAGE
-  };
+  });
 };
-
-export const 
 
 export const submitAnswer = (stage, answer) => ({
   type: SUBMIT_ANSWER,
