2c41cb9845a65cf4da59ea52247addd2709ee557
sim8 <simeon.k.lees@gmail.com>
2019-10-22 20:25:53 -0400

    Kill tracking middleware and add tracking to redux actions

--- actions/progressActions.js
+++ actions/progressActions.js
@@ -3,14 +3,32 @@ import {
   MOVE_TO_PREVIOUS_STAGE,
   SUBMIT_ANSWER
 } from "./actionTypes";
+import {
+  ACTIONS as TRACKING_ACTIONS,
+  CATEGORIES as TRACKING_CATEGORIES
+} from "../constants/trackingConstants";
 
-export const moveToNextStage = () => ({
-  type: MOVE_TO_NEXT_STAGE
-});
+export const moveToNextStage = () => {
+  event(TRACKING_ACTIONS.MOVE_TO_NEXT_STAGE, {
+    category: TRACKING_CATEGORIES.STAGE,
+    label: stageId
+  });
+  return {
+    type: MOVE_TO_NEXT_STAGE
+  };
+};
 
-export const moveToPreviousStage = () => ({
-  type: MOVE_TO_PREVIOUS_STAGE
-});
+export const moveToPreviousStage = () => {
+  event(TRACKING_ACTIONS.MOVE_TO_PREVIOUS_STAGE, {
+    category: TRACKING_CATEGORIES.STAGE,
+    label: stageId
+  });
+  return {
+    type: MOVE_TO_PREVIOUS_STAGE
+  };
+};
+
+export const 
 
 export const submitAnswer = (stage, answer) => ({
   type: SUBMIT_ANSWER,
