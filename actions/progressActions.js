7115baf7cd295b789a555596469a74b74326a307
sim8 <simeon.k.lees@gmail.com>
2020-08-29 16:36:33 +0100

    Add deps

--- actions/progressActions.js
+++ actions/progressActions.js
@@ -1,23 +1,23 @@
 import {
   MOVE_TO_NEXT_STAGE,
   MOVE_TO_PREVIOUS_STAGE,
-  SUBMIT_ANSWER
-} from "./actionTypes";
+  SUBMIT_ANSWER,
+} from './actionTypes';
 import {
   ACTIONS as TRACKING_ACTIONS,
-  CATEGORIES as TRACKING_CATEGORIES
-} from "../constants/trackingConstants";
-import { getStageId } from "../selectors/progressSelectors";
-import { event } from "../lib/gtag";
+  CATEGORIES as TRACKING_CATEGORIES,
+} from '../constants/trackingConstants';
+import { getStageId } from '../selectors/progressSelectors';
+import { event } from '../lib/gtag';
 
 export const moveToNextStage = () => (dispatch, getState) => {
   const stageId = getStageId(getState());
   event(TRACKING_ACTIONS.MOVE_TO_NEXT_STAGE, {
     category: TRACKING_CATEGORIES.STAGE,
-    label: stageId
+    label: stageId,
   });
   dispatch({
-    type: MOVE_TO_NEXT_STAGE
+    type: MOVE_TO_NEXT_STAGE,
   });
 };
 
@@ -25,15 +25,15 @@ export const moveToPreviousStage = () => (dispatch, getState) => {
   const stageId = getStageId(getState());
   event(TRACKING_ACTIONS.MOVE_TO_PREVIOUS_STAGE, {
     category: TRACKING_CATEGORIES.STAGE,
-    label: stageId
+    label: stageId,
   });
   dispatch({
-    type: MOVE_TO_PREVIOUS_STAGE
+    type: MOVE_TO_PREVIOUS_STAGE,
   });
 };
 
 export const submitAnswer = (stage, answer) => ({
   type: SUBMIT_ANSWER,
   stage,
-  answer
+  answer,
 });
