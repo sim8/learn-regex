4fc934474dbfa0b2a05c184d6087d489c367d785
Simeon Lees <simeon.k.lees@gmail.com>
2023-02-21 19:30:50 +0000

    create-next-app

--- reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import input from './input';
-import moduleProgress from './moduleProgress';
-import overallProgress from './overallProgress';
-
-export default combineReducers({ input, moduleProgress, overallProgress });
