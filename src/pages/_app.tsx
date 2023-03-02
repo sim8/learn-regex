a6371b42ac7932b7f3b706fd62cc2603e459de88
Simeon Lees <simeon.k.lees@gmail.com>
2023-03-02 21:33:41 +0000

    Use Next google fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,22 @@
 import '../styles/globals.css';
+import { Ubuntu_Mono } from '@next/font/google';
 
 import { Provider } from 'react-redux';
 import type { AppProps } from 'next/app';
 
 import store from '../store';
 
+const ubuntuMono = Ubuntu_Mono({
+  weight: '400',
+  subsets: ['latin'],
+});
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <main className={`${ubuntuMono.className}`}>
+        <Component {...pageProps} />
+      </main>
     </Provider>
   );
 }
