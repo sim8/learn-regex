8f386e1dcc7bb922319cd32c8bd917b28ab228f5
Simeon Lees <simeon.k.lees@gmail.com>
2023-03-05 14:52:59 +0000

    Fix typo

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -10,17 +10,18 @@ export const pageview = (url: string) => {
 };
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({
-  action,
-  category,
-  label,
-  value,
-}: {
-  action: string;
-  category?: string;
-  label?: string;
-  value?: number;
-}) => {
+export const event = (
+  action: string,
+  {
+    category,
+    label,
+    value,
+  }: {
+    category?: string;
+    label?: string;
+    value?: number;
+  } = {}
+) => {
   window.gtag('event', action, {
     event_category: category,
     event_label: label,
