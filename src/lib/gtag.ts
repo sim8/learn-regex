84fe88ead59b171e60890e794f9f2a5f2ea89394
Simeon Lees <simeon.k.lees@gmail.com>
2023-03-05 14:47:31 +0000

    Implement gtag

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,3 +1,29 @@
-export const event = (...args: any[]) => {
-  console.log('todo');
+/* global window */
+
+import { GA_TRACKING_ID } from '../constants/trackingConstants';
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: string) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = ({
+  action,
+  category,
+  label,
+  value,
+}: {
+  action: string;
+  category?: string;
+  label?: string;
+  value?: number;
+}) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value,
+  });
 };
